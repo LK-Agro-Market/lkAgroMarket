ecd3de15ba536969e01c72d6967ec3beefe0f81c
ravinduramesh <ravinduramesh@gmail.com>
2019-12-04 10:46:11 +0530

    fixed errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,9 @@ import {
   NbButtonModule,
   NbCardModule,
   NbMenuModule,
-  NbActionsModule,
-  NbToastrModule
+  NbActionsModule
 } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { NotifierModule, NotifierOptions } from 'angular-notifier';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,47 +34,6 @@ import { RegistrationGuard } from './shared/guards/registration.guard';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { RegiReverseGuard } from './shared/guards/regi-reverse.guard';
 import { ToastrModule } from 'ngx-toastr';
-
-const customNotifierOptions: NotifierOptions = {
-  position: {
-    horizontal: {
-      position: 'right',
-      distance: 12
-    },
-    vertical: {
-      position: 'top',
-      distance: 12,
-      gap: 10
-    }
-  },
-  theme: 'material',
-  behaviour: {
-    autoHide: 3000,
-    onClick: 'hide',
-    onMouseover: 'pauseAutoHide',
-    showDismissButton: true,
-    stacking: 4
-  },
-  animations: {
-    enabled: true,
-    show: {
-      preset: 'slide',
-      speed: 300,
-      easing: 'ease'
-    },
-    hide: {
-      preset: 'fade',
-      speed: 300,
-      easing: 'ease',
-      offset: 50
-    },
-    shift: {
-      speed: 300,
-      easing: 'ease'
-    },
-    overlap: 150
-  }
-};
 
 @NgModule({
   declarations: [
@@ -105,8 +62,7 @@ const customNotifierOptions: NotifierOptions = {
     NbEvaIconsModule,
     NbMenuModule.forRoot(),
     NbActionsModule,
-    ToastrModule.forRoot(),
-    NotifierModule.withConfig(customNotifierOptions)
+    ToastrModule.forRoot()
   ],
   providers: [
     AuthGuard,
