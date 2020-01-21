b9d2fb425352110fe5ab149ce6ecafe7eb61af7f
banura96 <mabhettiarachchi@gmail.com>
2020-01-22 00:41:32 +0530

    modify8

--- src/app/shared/models/comment-ad.ts
+++ src/app/shared/models/comment-ad.ts
@@ -1,9 +1,9 @@
-export class Comment{
-    adId
-    updatedAt
-    userName;
-    date: Date = new Date();
-    content: string;
-    docId: string;
-    docPath: string = "";
+export class Comment {
+  adId;
+  updatedAt;
+  userName;
+  date: Date = new Date();
+  content: string;
+  docId: string;
+  docPath: string = '';
 }
