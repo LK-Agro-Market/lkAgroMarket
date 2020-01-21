20239bb28d6679cfcd530c91c244a0298ac62187
banura96 <mabhettiarachchi@gmail.com>
2020-01-22 00:26:40 +0530

    modify3

--- src/app/dashboard/demand-ad/view-demand-ad/comment-buyer-ad/comment-buyer-ad.component.ts
+++ src/app/dashboard/demand-ad/view-demand-ad/comment-buyer-ad/comment-buyer-ad.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentService} from 'src/app/dashboard/demand-ad/view-demand-ad/services/firebaseservice/comment.service';
+import { Comment } from 'src/app/shared/models/comment-ad';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-comment-buyer-ad',
@@ -6,7 +11,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment-buyer-ad.component.scss']
 })
 export class CommentBuyerAdComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  user: User = JSON.parse(localStorage.getItem('user'));
+  comments$: Observable<any[]>;
+
+  constructor(private commentservice:CommentService,
+    private route: ActivatedRoute
+    ) 
+    {
+    this.comment = new Comment();
+    this.viewReply = "";
+    }
+
+cmt:any;
+
+COMMENTS_REF: string = "comments";
+COL_NODE: string;
+
+viewReply: string;
+editComment: string;
+
+adId = this.getadId();
+  ngOnInit() {
+    this.cmt=this.commentservice.getadds(this.adId)
+  }
+
+  onComment(comment) {
+    this.commentservice.add(comment)
+      .then(_ => {
+        this.comment= new Comment();
+      })
+  }
+
+  getadId(){
+    let id = this.route.snapshot.paramMap.get('demandAdid');
+    return id;
+  }
+ 
+
+  onEdit(comment: Comment) {
+    this.commentservice.update(comment.docId, comment)
+      .then(_ => {
+        this.editComment = "";
+      })
+  }
+
+
+  onDelete(pass) {
+    this.commentservice.delete(pass);
+  }
+
+  comment:Comment={
+  adId : this.getadId(),
+  updatedAt:'',
+  userName: this.user.displayName,
+  date: new Date(),
+  content: '',
+  docId: '',
+  docPath:''
+}
+
+  on(comm){
+  const comment: Comment={
+    adId: this.getadId(),
+    updatedAt:'',
+    userName: this.user.displayName,
+    date: new Date(),
+    content:comm.content,
+    docId: '',
+    docPath: '',
+  }
+   this.onComment(comment);
+}
+
 }
