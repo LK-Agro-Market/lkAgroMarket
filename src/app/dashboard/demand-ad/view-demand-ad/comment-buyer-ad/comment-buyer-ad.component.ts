850cab8cb5d3d61e841e35511472aaae55a81c5a
banura96 <mabhettiarachchi@gmail.com>
2020-01-22 00:43:59 +0530

    modify12

--- src/app/dashboard/demand-ad/view-demand-ad/comment-buyer-ad/comment-buyer-ad.component.ts
+++ src/app/dashboard/demand-ad/view-demand-ad/comment-buyer-ad/comment-buyer-ad.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CommentService} from 'src/app/dashboard/demand-ad/view-demand-ad/services/firebaseservice/comment.service';
+import { CommentService } from 'src/app/dashboard/demand-ad/view-demand-ad/services/firebaseservice/comment.service';
 import { Comment } from 'src/app/shared/models/comment-ad';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
@@ -11,77 +11,71 @@ import { User } from 'src/app/shared/models/user';
   styleUrls: ['./comment-buyer-ad.component.scss']
 })
 export class CommentBuyerAdComponent implements OnInit {
-
   user: User = JSON.parse(localStorage.getItem('user'));
   comments$: Observable<any[]>;
 
-  constructor(private commentservice:CommentService,
+  constructor(
+    private commentservice: CommentService,
     private route: ActivatedRoute
-    ) 
-    {
+  ) {
     this.comment = new Comment();
-    this.viewReply = "";
-    }
+    this.viewReply = '';
+  }
 
-cmt:any;
+  cmt: any;
 
-COMMENTS_REF: string = "comments";
-COL_NODE: string;
+  COMMENTS_REF: string = 'comments';
+  COL_NODE: string;
 
-viewReply: string;
-editComment: string;
+  viewReply: string;
+  editComment: string;
 
-adId = this.getadId();
+  adId = this.getadId();
   ngOnInit() {
-    this.cmt=this.commentservice.getadds(this.adId)
+    this.cmt = this.commentservice.getadds(this.adId);
   }
 
   onComment(comment) {
-    this.commentservice.add(comment)
-      .then(_ => {
-        this.comment= new Comment();
-      })
+    this.commentservice.add(comment).then(_ => {
+      this.comment = new Comment();
+    });
   }
 
-  getadId(){
+  getadId() {
     let id = this.route.snapshot.paramMap.get('demandAdid');
     return id;
   }
- 
 
   onEdit(comment: Comment) {
-    this.commentservice.update(comment.docId, comment)
-      .then(_ => {
-        this.editComment = "";
-      })
+    this.commentservice.update(comment.docId, comment).then(_ => {
+      this.editComment = '';
+    });
   }
-
 
   onDelete(pass) {
     this.commentservice.delete(pass);
   }
 
-  comment:Comment={
-  adId : this.getadId(),
-  updatedAt:'',
-  userName: this.user.displayName,
-  date: new Date(),
-  content: '',
-  docId: '',
-  docPath:''
-}
-
-  on(comm){
-  const comment: Comment={
+  comment: Comment = {
     adId: this.getadId(),
-    updatedAt:'',
+    updatedAt: '',
     userName: this.user.displayName,
     date: new Date(),
-    content:comm.content,
+    content: '',
     docId: '',
-    docPath: '',
-  }
-   this.onComment(comment);
-}
+    docPath: ''
+  };
 
+  on(comm) {
+    const comment: Comment = {
+      adId: this.getadId(),
+      updatedAt: '',
+      userName: this.user.displayName,
+      date: new Date(),
+      content: comm.content,
+      docId: '',
+      docPath: ''
+    };
+    this.onComment(comment);
+  }
 }
